9531121a876590bde81effa4f5d677276854231e
ShapeOfYOuG <axgx.gb@gmail.com>
2023-02-14 13:53:27 +0300

    media

--- js/script.js
+++ js/script.js
@@ -9,12 +9,12 @@ x.play();
 
 nob.addEventListener("mouseenter", moveHover);
 yesb.addEventListener("click", () => {
-  question.innerHTML = "ركبني المرجيحة";
-  question1.innerHTML = "ركبني المرجيحة";
+  question.innerHTML = "2028/06/21♥";
+  question1.innerHTML = "2028/06/21♥";
 });
 function moveHover() {
   const i = Math.floor(Math.random() * 200) + 1;
-  const j = Math.floor(Math.random() * 200) + 1;
+  const j = Math.floor(Math.random() * 250) + 1;
 
   nob.style.right = i + "px";
   nob.style.top = j + "px";
